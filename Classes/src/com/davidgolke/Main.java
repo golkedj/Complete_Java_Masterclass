f08baeb32a87d0c9e87de57c735c0209d14d5ff1
golkedj <golkedj@gmail.com>
2017-05-25 20:32:18 -0500

    Section: 7, Lecture 38

--- Classes/src/com/davidgolke/Main.java
+++ Classes/src/com/davidgolke/Main.java
@@ -3,8 +3,9 @@ package com.davidgolke;
 public class Main {
 
     public static void main(String[] args) {
-	    Car porsche = new Car();
-	    Car holden = new Car();
-//	    porsche.model = "Carrera";
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("911");
+        System.out.println("Model is " + porsche.getModel());
     }
 }
