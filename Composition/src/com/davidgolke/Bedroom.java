9a0d80f17de39406fa5e602d26cc05a0dc289418
golkedj <golkedj@gmail.com>
2017-05-28 11:21:05 -0500

    Section 8, Lecture 47 - My Solution

--- Composition/src/com/davidgolke/Bedroom.java
+++ Composition/src/com/davidgolke/Bedroom.java
@@ -5,10 +5,11 @@ package com.davidgolke;
  */
 public class Bedroom {
     private Bed bed = new Bed("Serta", 50);
-    private Ceiling ceiling = new Ceiling();
-    private Lamp lamp = new Lamp();
-    private Wall frontWall = new Wall();
-    private Wall backWall = new Wall();
-    private Wall leftWall = new Wall();
-    private Wall rightWall = new Wall();
+    private Ceiling ceiling = new Ceiling(12);
+    private Lamp lamp = new Lamp("blue", 2, false);
+    private Wall frontWall = new Wall(12, 20, "white");
+    private Wall backWall = new Wall(12, 20, "white");
+    private Wall leftWall = new Wall(12, 20, "white");
+    private Wall rightWall = new Wall(12, 20, "white");
+    
 }
