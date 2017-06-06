0a4ef6eb4eec397e2273520bd465ff9e6ce6f6fd
golkedj <golkedj@gmail.com>
2017-06-06 18:07:54 -0500

    Section 8, Lecture 53 - Course Solution

--- OOPTest/src/com/davidgolke/DeluxeBurger.java
+++ OOPTest/src/com/davidgolke/DeluxeBurger.java
@@ -3,6 +3,30 @@ package com.davidgolke;
 /**
  * Created by David Golke on 6/6/2017.
  */
-public class DeluxeBurger {
+public class DeluxeBurger extends Hamburger {
+    public DeluxeBurger() {
+        super("Deluxe", "Sausage & Bacon", 14.54, "White");
+        super.addHamburgerAddition1("Chips", 2.75);
+        super.addHamburgerAddition2("Drink", 1.81);
+    }
 
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
 }
